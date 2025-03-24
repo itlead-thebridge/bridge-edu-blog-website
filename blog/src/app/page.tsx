b985730459fa02c8c9b4b81a5bebb573e0b7dda1
Pavel Belov <pbelov@jumpmind.com>
2025-03-24 08:00:21 -0400

    HomepageIntro on the main page

--- blog/src/app/page.tsx
+++ blog/src/app/page.tsx
@@ -1,14 +1,15 @@
 
-// import SiteHeader from '@/components/SiteHeader';
-// import SiteFooter from '@/components/SiteFooter';
+import HomepageIntro from '@/components/HomepageIntro';
 import NoteSiteMaintenance from "@/components/NoteSiteMaintenance";
 
 export default function Home() {
+//    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">      
+//    </div>
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">      
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start"> 
+      <main className="flex flex-col p-4 pb-4 gap-4 row-start-2 items-center sm:items-start"> 
+        <HomepageIntro/>
         <NoteSiteMaintenance message="Site is under construction (we are changing providers). Please check back soon."/>
       </main>      
-    </div>
   );
 }
